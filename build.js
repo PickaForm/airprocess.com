a2e89983e0b67f82aafa560cc6621f37306e7879
David Grossi <david.grossi@pickaform.com>
2025-08-04 14:59:14 +0400

    Fixed app links to point to previous pickaform URLs

--- build.js
+++ build.js
@@ -59,10 +59,10 @@ kiss.theme.set({
 // Paths
 kiss.global.path = `https://${window.location.host}`
 kiss.global.pathImg = "./resources/img"
-kiss.global.pathAirProcess = `https://app.airprocess.com`
+kiss.global.pathAirProcess = `https://app.pickaform.com`
 
 // Blog
-kiss.global.blogEndPoint = "https://app.airprocess.com/command/blog"
+kiss.global.blogEndPoint = "https://app.pickaform.com/command/blog"
 kiss.global.blogModelId = "0187ed51-d3a5-70ea-869c-6c538d786fb7"
 kiss.global.blogPostTitle = "y9yVRPEQ"
 kiss.global.blogPostDescription = "BedquzD8"
@@ -73,7 +73,7 @@ kiss.global.blogPostPublished = "rhI4E1iH"
 kiss.global.contactModelId = "0187b40b-0061-7f65-af86-982a361afcf3"
 
 // AI Art
-kiss.global.artEndPoint = "https://beta.airprocess.com/command/product"
+kiss.global.artEndPoint = "https://beta.pickaform.com/command/product"
 kiss.global.artModelId = "01889cf0-5878-7352-93b5-3a0fb88c852f"
 kiss.global.artTitle = "pJZ5QvWL"
 kiss.global.artPulished = "DgllE0KD"
